0aa820a522e8f4cf64f13c73068d1eef12866965
tonkaxxx <maltsev.yar@gmail.com>
2026-09-01 22:03:56 +0300

    01.09.26 laptop

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -209,6 +209,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.settings.X11Forwarding = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
